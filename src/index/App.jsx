f0ba8b82f721966b375d40ecc67e6b590349d184
duanzihuang <huangwei@itcast.cn>
2019-10-07 21:40:32 +0800

    8-完成了始发站和终点站的切换功能

--- src/index/App.jsx
+++ src/index/App.jsx
@@ -1,6 +1,6 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useMemo } from 'react'
 import { connect } from 'react-redux'
-// import './App.css'
+import './App.css'
 
 import Header from '../common/Header'
 import Journey from './Journey'
@@ -8,10 +8,43 @@ import DepartDate from './DepartDate'
 import HignSpeed from './HignSpeed'
 import Submit from './Submit'
 
+import { bindActionCreators } from 'redux'
+
+import { exchangeFromTo, showCitySelector } from './actions'
+
 function App(props) {
+  const { from, to, dispatch } = props
+
   // 使用 useCallback 避免Header不必要的重渲染
   const onBack = useCallback(() => {
     window.history.back()
+  }, [])
+
+  /**
+   * useCallback
+    const doExchangeFromTo = useCallback(() => {
+      dispatch(exchangeFromTo())
+    },[])
+
+    const doShowCitySelector = useCallback(m => {
+      dispatch(showCitySelector(m))
+    },[])
+   */
+
+  /**
+   * useMemo
+   *
+   * useCallback & useMemo
+   * https://blog.csdn.net/sinat_17775997/article/details/94453167
+   */
+  const cbs = useMemo(() => {
+    return bindActionCreators(
+      {
+        exchangeFromTo,
+        showCitySelector
+      },
+      dispatch
+    )
   }, [])
 
   return (
@@ -19,19 +52,26 @@ function App(props) {
       <div className="header-wrapper">
         <Header onBack={onBack} title="火车票" />
       </div>
-      <Journey />
-      <DepartDate />
-      <HignSpeed />
-      <Submit />
+      <form className="form">
+        {/* 使用 useCallBack */}
+        {/* <Journey from={from} to={to} exchangeFromTo={doExchangeFromTo} showCitySelector={doShowCitySelector}/> */}
+
+        {/* 使用 useMemo  */}
+        <Journey from={from} to={to} {...cbs} />
+
+        <DepartDate />
+        <HignSpeed />
+        <Submit />
+      </form>
     </div>
   )
 }
 
 export default connect(
   function mapStateToProps(state) {
-    return {}
+    return state
   },
   function mapDispatchToProps(dispatch) {
-    return {}
+    return { dispatch }
   }
 )(App)
--- src/index/index.js
+++ src/index/index.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
 // import 'normalize.css/normalize.css'
 
 import store from './store'
 
-// import './index.css'
+import './index.css'
 
 import App from './App'
 
-ReactDOM.render(<Provider store={store}><App /></Provider>,document.getElementById('root'))
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
